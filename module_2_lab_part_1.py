96d728b0fb1da94c7f87505e3a8445ae0fcd7620
ARTemporary <alice.radtke@go.minneapolis.edu>
2025-06-28 21:20:11 -0500

    Add files via upload

---
+++ module_2_lab_part_1.py
@@ -0,0 +1,36 @@
+# Name: Alice Radtke
+# Module 2 Lab Part 1 - Basics
+
+## Variables and Basic Types
+print("Variables:\n")
+age = 24
+pi = 3.14159
+name = "Alice"
+is_student = True
+
+print(age)
+print("Pi:" , pi)
+print("Name:" , name)
+print("Is student?" , is_student)
+
+## Basic Math
+a = 10
+b = 5
+print("Math:\n")
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+
+##String Opporations
+
+print("Opporations:\n")
+
+first_name = "Alice"
+last_name = "Radtke"
+full_name = first_name + " " + last_name
+
+print(full_name)
+
+username = input("Enter your name: ")
+print ("Hello, " + username + "!")
---
+++ module_2_lab_part_2_fortune_cookie.py
@@ -0,0 +1,17 @@
+# Module 2 Lab Part 2 Challenge
+# Name: Alice Radtke
+# Description: Fortune Cookie App - displays personalized fortun for users
+
+print()
+user_name = input("Please enter your name: \n")
+
+# print("\nHello, " + user_name)
+print()
+
+fortune = "Watch out for Mondays!"
+
+print("Your fortune cookie says:")
+print(fortune, "\n")
+
+
+print("Thank you for using the Fortune Cookie App,", user_name + "!")
